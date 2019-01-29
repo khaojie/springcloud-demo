ad84ffdd033d96098ab6179006e84661e14e08c5
khaojie <haojie.kang@jhj.com.cn>
2019-01-29 14:04:10 +0800

    aaa

--- serviceZipkin/src/main/java/com/springcloud/demo/zipkin/ZipkinApplication.java
+++ serviceZipkin/src/main/java/com/springcloud/demo/zipkin/ZipkinApplication.java
@@ -2,13 +2,24 @@ package com.springcloud.demo.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class,args);
+    }
+
+    @Bean
+    @Primary
+    public MySQLStorage mySQLStorage(DataSource dataSource) {
+        return MySQLStorage.builder().datasource(dataSource).executor(Runnable::run).build();
     }
 }
